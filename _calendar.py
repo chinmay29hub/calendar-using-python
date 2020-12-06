6387e0cfbfdae62a43324d9446afb76c949d6bcc
Chinmay Sonawane <69494946+chinmay29hub@users.noreply.github.com>
2020-12-06 10:22:36 +0530

    Update _calendar.py
    user input

--- _calendar.py
+++ _calendar.py
@@ -1,7 +1,8 @@
 import calendar
 
-yy = 2020 # year
-mm = 11   # month
+yy = int(input("Enter the year  -------> "))    # year
+mm = int(input("Enter the month -------> "))   # month
+print("\n\n")
 
 
 print(calendar.month(yy, mm))
